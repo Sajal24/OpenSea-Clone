a01b856d740d81534d1575e4a6d1e839c8666617
Sajal Sharma <sajalsharma2408@gmail.com>
2022-11-04 13:23:42 +0530

    yesterday's missery solved fuckin shit

---
+++ backend/contracts/NFT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+//declaring our contract and imports
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+
+
+
